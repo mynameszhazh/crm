4cd8d136f27ae52d81a98212fdc0c7aa2bf4946d
mynameszhazh <1594776259@qq.com>
2023-08-30 23:36:02 +0800

    ci: 完成市场活动的创建功能

--- crm/src/main/java/com/bjpowernode/crm/settings/service/impl/UserServiceImpl.java
+++ crm/src/main/java/com/bjpowernode/crm/settings/service/impl/UserServiceImpl.java
@@ -22,6 +22,6 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> queryAllUsers() {
-        return null;
+        return userMapper.selectAllUsers();
     }
 }
