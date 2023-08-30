83468da76a9e902c61a853937e5d1e06f93041eb
mynameszhazh <1594776259@qq.com>
2023-08-30 23:35:19 +0800

    ci: 完成市场活动的创建功能

--- crm/src/main/java/com/bjpowernode/crm/workbench/web/controller/ActivityController.java
+++ crm/src/main/java/com/bjpowernode/crm/workbench/web/controller/ActivityController.java
@@ -1,12 +1,21 @@
 package com.bjpowernode.crm.workbench.web.controller;
 
+import com.bjpowernode.crm.commons.contants.Contants;
+import com.bjpowernode.crm.commons.domain.ReturnObject;
+import com.bjpowernode.crm.commons.utils.DateUtils;
+import com.bjpowernode.crm.commons.utils.UUIDUtils;
 import com.bjpowernode.crm.settings.domain.User;
 import com.bjpowernode.crm.settings.service.UserService;
+import com.bjpowernode.crm.workbench.domain.Activity;
+import com.bjpowernode.crm.workbench.service.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -14,6 +23,10 @@ public class ActivityController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private ActivityService activityService;
+
 
     @RequestMapping("/workbench/activity/index.do")
     public String index(HttpServletRequest request){
@@ -23,5 +36,29 @@ public class ActivityController {
         request.setAttribute("userList",userList);
         //请求转发到市场活动的主页面
         return "workbench/activity/index";
+    }
+
+    @RequestMapping("/workbench/activity/saveCreateActivity.do")
+    public @ResponseBody Object saveCreateActivity(Activity activity, HttpSession session) {
+        User user = (User) session.getAttribute(Contants.SESSION_USER);
+        activity.setId(UUIDUtils.getUUID());
+        activity.setCreateTime(DateUtils.formatDateTime(new Date()));
+        activity.setCreateBy(user.getId());
+
+        ReturnObject returnObject = new ReturnObject();
+        try {
+            int ret = activityService.saveCreateActivity(activity);
+            if(ret > 0) {
+                returnObject.setCode(Contants.RESULT_OBJECT_SUCCESS_CODE);
+            } else  {
+                returnObject.setCode(Contants.RESULT_OBJECT_ERROR_CODE);
+                returnObject.setMessage("系统忙, 请稍后重试...");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            returnObject.setCode(Contants.RESULT_OBJECT_ERROR_CODE);
+            returnObject.setMessage("系统忙, 请稍后重试...");
+        }
+        return returnObject;
     }
 }
